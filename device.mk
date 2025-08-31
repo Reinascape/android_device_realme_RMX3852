0bd455e7633886ea20ebd016b35429ded91321b3
Andrew <87061244+MisterZtr@users.noreply.github.com>
2025-08-31 15:15:27 +0300

    RMX3852: Return AudioFX
    After adding audio effects to the category for cliffs, the equalizers are working again

--- device.mk
+++ device.mk
@@ -49,9 +49,6 @@ $(call soong_config_set,OPLUS_LINEAGE_VIBRATOR_HAL,INCLUDE_DIR,$(LOCAL_PATH)/vib
 # OMX
 TARGET_SUPPORTS_OMX_SERVICE := false
 
-# Disable AudioFX
-TARGET_EXCLUDES_AUDIOFX := true
-
 # Inherit from the common OEM chipset makefile.
 $(call inherit-product, device/realme/sm8635-common/common.mk)
 
