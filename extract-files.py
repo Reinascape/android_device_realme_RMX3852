5d0cc1dcbf91dc838b5ef17aa344e4f1a8037d04
Michael Bestas <mkbestas@lineageos.org>
2025-11-21 07:10:09 +0530

    RMX3852: Depend on android.hardware.graphics.common-V7
    Required for BP3A.
    
    Change-Id: Id071b20aa56d5c2e2717215fa6bb6f16f91198ec

--- extract-files.py
+++ extract-files.py
@@ -61,8 +61,8 @@ blob_fixups: blob_fixups_user_type = {
     'odm/etc/camera/CameraHWConfiguration.config': blob_fixup()
         .regex_replace('SystemCamera =  0;  0;  0;  1;  0; 1;', 'SystemCamera =  0;  0;  0;  0;  0; 0;'),
     'odm/lib64/libAlgoProcess.so': blob_fixup()
-        .replace_needed('android.hardware.graphics.common-V3-ndk.so', 'android.hardware.graphics.common-V6-ndk.so')
-        .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V6-ndk.so'),
+        .replace_needed('android.hardware.graphics.common-V3-ndk.so', 'android.hardware.graphics.common-V7-ndk.so')
+        .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
     ('odm/lib64/libCOppLceTonemapAPI.so', 'odm/lib64/libYTCommon.so', 'odm/lib64/libSuperRaw.so', 'odm/lib64/libyuv2.so'): blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
     ('odm/lib64/libEIS.so', 'odm/lib64/libEISLive.so', 'odm/lib64/libHIS.so', 'odm/lib64/libOPAlgoCamFaceBeautyCap.so', 'odm/lib64/libOGLManager.so', 'odm/lib64/libAncHumanSegFigureFusion.so', 'odm/lib64/libOPAlgoCamAiBeautyFaceRetouchCn.so'): blob_fixup()
