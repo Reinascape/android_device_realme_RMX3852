b5c375fdeb02a11994bd9d6c231b7476a71832c4
Yumi Yukimura <me.cafebabe@gmail.com>
2025-12-10 05:30:25 +0000

    RMX3852: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57

--- device.mk
+++ device.mk
@@ -44,7 +44,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # Touch features
-$(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_GM,true)
+$(call soong_config_set_bool,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_GM,true)
 
 # Vibrator
 PRODUCT_PACKAGES += \
@@ -66,7 +66,7 @@ PRODUCT_PACKAGES += \
     android.hardware.weaver-service.nxp
 
 # LiveDisplay
-$(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
+$(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
 # Inherit from the common OEM chipset makefile.
 $(call inherit-product, device/realme/sm8635-common/common.mk)
