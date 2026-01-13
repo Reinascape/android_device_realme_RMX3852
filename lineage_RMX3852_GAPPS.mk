8f38f394475c7e8bdf29edfebf6026261e2a8498
rscape <reinascape@gmail.com>
2026-01-13 20:30:53 +0300

    RMX3852: Setup EvolutionX build flags

--- lineage_RMX3852_GAPPS.mk
+++ lineage_RMX3852_GAPPS.mk
@@ -1,6 +1,9 @@
 # Inherit from RMX3852 main mk
 $(call inherit-product, device/realme/RMX3852/RMX3852.mk)
 
+# EvolutionX build flags
+TARGET_HAS_UDFPS := true
+
 PRODUCT_NAME := lineage_RMX3852_GAPPS
 PRODUCT_DEVICE := RMX3852
 PRODUCT_MANUFACTURER := realme
